9cef2034a667ca1e038bd4cfde4deed21cc7ab2c
ashwin1489 <ashw5325@gmail.com>
2026-02-03 23:36:02 +0530

    Time: 6 ms (99.25%), Space: 49.8 MB (59.84%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+          // Step 1: Create HashMap
+        Map<String,List<String>>map  = new HashMap<>();
+
+        // Step 2: Loop through each word
+        for(String word:strs){
+            char[] chars = word.toCharArray();
+
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(word);
+
+
+
+        }
+        return new ArrayList<>(map.values());
+
+
+        
+    }
+}
